4d7bfcbe38ac0367490cfe1c81f782a8892c8713
lisa <lisa@odinsoftware.co.kr>
2017-08-14 16:12:38 +0900

    .net standard 2.0

---
+++ src/DotNet.Push.Sample.Core.c20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DotNet.Push.Core.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _test_phone = "IX";
+
+            // IPHONE
+            if (_test_phone == "IA")
+            {
+                var _apns = new IosPushNotifyAPNs("<key-id>", "<team-id>", "<app-id>", "<key-file-path>");
+                var _result = _apns
+                                .JwtAPNsPush("<device-token>", "Json Web Token(JWT): Apple Push Notification Service(APNs)", 1, "<sound>")
+                                .GetAwaiter()
+                                .GetResult();
+
+                Console.WriteLine($"result: {_result.success}, '{_result.message}'");
+            }
+
+            // ANDROID
+            if (_test_phone == "AF")
+            {
+                var _fcm = new DroidPushNotifyFCM("<server-api-key>", "<server-id>", "<alarm-tag>");
+                var _result = _fcm
+                                .SendNotification("<to>", "<priority>", "<title>", "<click-action>", "<message>", 1, "<icon-name>", "<color>")
+                                .GetAwaiter()
+                                .GetResult();
+
+                Console.WriteLine($"result: {_result.success}, '{_result.message}'");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
